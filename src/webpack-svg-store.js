9639a85d3c13ae6accbe75ca74b6493cdaaac504
Jordi Kroon <jkroon@tde.nl>
2017-12-04 11:20:15 +0100

    Initial commit

---
+++ src/webpack-svg-store.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+const SVGSpriter = require('svg-sprite');
+const mkdirp = require('mkdirp');
+
+/**
+ * @param inputPath
+ * @param options
+ * @constructor
+ */
+const WebpackSvgStore = function (inputPath, options) {
+    this.inputPath = inputPath;
+    this.spriter = new SVGSpriter(options);
+};
+
+/**
+ * @param compiler
+ */
+WebpackSvgStore.prototype.apply = function (compiler) {
+    let that = this;
+
+    compiler.plugin('emit', function (compilation, callback) {
+        glob(that.inputPath + '/**/*.svg', function (err, files) {
+            for (let fileKey in files) {
+                that.spriter.add(files[fileKey], null, fs.readFileSync(files[fileKey], {encoding: 'utf-8'}));
+            }
+
+            that.spriter.compile(function(error, result) {
+                for (let mode in result) {
+                    for (let resource in result[mode]) {
+                        mkdirp.sync(path.dirname(result[mode][resource].path));
+                        fs.writeFileSync(result[mode][resource].path, result[mode][resource].contents);
+                    }
+                }
+
+                callback();
+            });
+        });
+    });
+};
+
+/**
+ * @type {WebpackSvgStore}
+ */
+module.exports = WebpackSvgStore;
